fc48ed6cf66e374a6f987df2363f18d6c9ca6b42
rafael_brito <rafaelbrito1305@gmail.com>
2023-09-13 17:11:51 -0500

    Nuevos cambios

---
+++ segunda/index.php
@@ -0,0 +1,63 @@
+ <!DOCTYPE hmtl>
+ <html>
+ <head>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="./css/bootstrap.min.css">
+ 	<title>TERROR´S</title>
+    <script>
+        
+    </script>
+
+    <style>
+       body{
+        background-image:url("./img/1920x1080-Dont-Breathe-2016-Horror-Movie-❤-4K-HD-Desktop-Wallpaper-para-4K-1024x576.jpg") ;
+        background-repeat: no-repeat;
+        background-position: center center;
+        background-attachment: fixed;
+    background-size: cover;
+       } 
+       .titulo{
+        border: black 0px solid;
+                position: relative;
+                left: 590px ; 
+                bottom:-80px ;
+       }
+    </style>
+
+ </head>
+ <body>
+ <h1 class="titulo">GUIÓN</h1>
+ <p>
+     
+ </p>
+
+ <div>
+
+  <ul class="pagination">
+    <li class="page-item disabled">
+      <a class="page-link" href="#">&laquo;</a>
+    </li>
+    <li class="page-item active">
+      <a class="page-link" href="http://localhost/ONDAS/index.php">1</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="http://localhost/ONDAS/segunda/index.php">2</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">3</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">4</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">5</a>
+    </li>
+    <li class="page-item">
+      <a class="page-link" href="#">&raquo;</a>
+    </li>
+  </ul>
+</div>
+
+ </body>
+ </html>
